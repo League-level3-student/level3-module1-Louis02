cb3b9752792a58c9e6b7af69e4cf9868f2851165
Hausen <45108867+Tacoalarmclock@users.noreply.github.com>
2019-03-13 16:53:33 -0700

    working on murder of crows

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -4,20 +4,42 @@ import java.util.ArrayList;
 
 public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> string = new ArrayList<String>();
+		// 2. Add five Strings to your list
+		string.add("Tom");
+		string.add("Zach");
+		string.add("Luka");
+		string.add("Joe");
+		string.add("Alex");
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < string.size(); i++) {
+			// System.out.println(string.get(i));
 
-		//2. Add five Strings to your list
+		}
+
+		// 4. Print all the Strings using a for-each loop
+		for (String s : string) {
+			// System.out.println(s);
+		}
+		// 5. Print only the even numbered elements in the list.
+
+		for (int i = 1; i < string.size(); i += 2) {
+			System.out.println(string.get(i));
+		}
+		System.out.println("   ");
+		// 6. Print all the Strings in reverse order.
+		for (int i = string.size() - 1; i >= 0; i--) {
+			System.out.println(string.get(i));
+		}
+		System.out.println("    ");
+		// 7. Print only the Strings that have the letter 'e' in them.
+		for (int i = 0; i < string.size(); i++) {
 		
-		//3. Print all the Strings using a standard for-loop
-		
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+			if(string.get(i).contains("e") == true) {
+				System.out.println(string.get(i));
+			}
+		}
 	}
 }
--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,15 +1,65 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names".
+	// When the add name button is clicked, display an input dialog that asks the
+	// user to enter a name. Add
+	// that name to an ArrayList. When the "View Names" button is clicked, display a
+	// message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
-	
-	
-	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton("Add Name");
+	JButton view = new JButton("View Names");
+	ArrayList<String> array = new ArrayList<String>();
+
+	public static void main(String[] args) {
+		new _02_GuestBook();
+	}
+	public _02_GuestBook () {
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(add);
+		panel.add(view);
+		frame.pack();
+		add();
+		view();
+
+	}
+
+	public void add() {
+		add.addActionListener(this);
+	}
+	public void view() {
+		view.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (add == e.getSource()) {
+			String name = JOptionPane.showInputDialog("PLease Enter Your Name");
+			array.add(name);
+		}
+		else if (view==e.getSource()) {
+			for(int i = 0; i <array.size(); i++) {
+			System.out.println("Guest Number "+ (i+1) + " "+  array.get(i));
+			}
+		}
+	}
+
 }
