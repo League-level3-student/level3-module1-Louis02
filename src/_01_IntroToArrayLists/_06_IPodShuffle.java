e47fce03757095c9c17879f9e1a833212b331592
Luki <>
2019-03-27 16:54:54 -0700

    starting on text undo redo

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,16 +1,26 @@
 package _01_IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 //Copyright The League of Amazing Programmers, 2015
+;
 
-public class _06_IPodShuffle{
+public class _06_IPodShuffle implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	ArrayList<Song> theSong = new ArrayList<Song>();
+	Song currentSong = null;
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				
-				
+
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -18,10 +28,36 @@ public class _06_IPodShuffle{
 		 * you're really cool, you can stop all the songs, before playing a new one on
 		 * subsequent button clicks.
 		 */
-		
+
+		theSong.add(new Song("Like_It.mp3"));
+		theSong.add(new Song("Lazy_Song.mp3"));
+		theSong.add(new Song("Panda.mp3"));
+		frame.add(panel);
+		panel.add(button);
+		frame.setVisible(true);
+		button.setText("Suprise Me");
+		button.addActionListener(this);
+		frame.pack();
+
 	}
-	
+
 	public static void main(String[] args) {
 		new _06_IPodShuffle();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (button == e.getSource()) {
+			Random ran = new Random();
+			int r = ran.nextInt(theSong.size());
+			if (currentSong!=null) {
+				currentSong.stop();
+			}
+			theSong.get(r).play();
+			currentSong = theSong.get(r);
+			System.out.println(theSong.get(r));
+			
+		}
 	}
 }
--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,15 +9,38 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double>doubles = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		for(int i = 0; i<100;i++) {
+			Random ran = new Random();
+		doubles.push(ran.nextDouble()*100);
+		}
 		
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		String number = JOptionPane.showInputDialog("Enter a number between 1 and 100");
+		String bumber = JOptionPane.showInputDialog("Enter another number between 1 and 100");
+		int num1 = Integer.parseInt(number);
+		int num2 = Integer.parseInt(bumber);
+		int bigger = 0;
+		int smaller = 0;
+		if(num1<num2) {
+			bigger = num2;
+			smaller = num1;
+		}
+		else if(num2<num1) {
+			bigger=num1;
+			smaller=num2;
+		}
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
-		
+		double currentPopped=1;
+		for(int i = 0; i<doubles.size();i++) {
+			currentPopped = doubles.pop();
+			if(currentPopped>smaller&&currentPopped<bigger) {
+				System.out.println(currentPopped);
+			}
+		}
+
 		//   EXAMPLE:
 		//   NUM 1: 65
 		//   NUM 2: 75
